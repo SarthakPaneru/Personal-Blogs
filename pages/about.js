91bc4a19b027497d7e3d7df408d414ddeccda03d
Sarthak <sarthak.paneru007@gmail.com>
2025-06-08 14:15:16 +0545

    feat: update about page content and links; change image source and personal information

--- pages/about.js
+++ pages/about.js
@@ -18,20 +18,17 @@ function about({ topics }) {
       <Navbar topics={topics} />
       <div className="max-w-2xl pt-20 pb-32 px-6 text-center mx-auto">
         <div className="h-64 w-full justify-center flex">
-          <img src="/about.png" className="h-64" />
+          <img src="/blogs/about.png" className="h-64" />
         </div>
         <h2 className="text-3xl font-semibold text-gray-700 py-4 dark:text-gray-200">
           Hi,{" "}
           <span className="bg-indigo-400 text-white rounded px-1 dark:bg-indigo-500">
-            I’m Soumyajit
+            I’m Sarthak
           </span>{" "}
           . Nice to meet you.
         </h2>
         <p className="text-gray-600 mt-4 dark:text-indigo-200">
-          I am a self taught developer currently pursuing IMSC in Maths and
-          Computing in BIT Mesra. My field of Interest's are building new Web
-          Technologies and Products and also in areas related to Deep Learning
-          and Natural Launguage Processing.
+          I’m a developer with a strong academic foundation from NCIT in Kathmandu, Nepal, and a passion for learning beyond the classroom. I enjoy building modern web technologies and products, and I’m always exploring new tools and techniques to improve performance and user experience.
         </p>
 
         <div className="text-center pt-8">
@@ -40,7 +37,7 @@ function about({ topics }) {
             <a
               rel="noopener noreferrer"
               target="_blank"
-              href="https://soumyajit.vercel.app/"
+              href="https://sarthakpaneru.com.np/"
             >
               See My Works
             </a>
@@ -49,10 +46,10 @@ function about({ topics }) {
           <button className="bg-indigo-500 px-3 py-1 font-semibold text-white inline-flex items-center space-x-2 rounded ml-3 dark:bg-indigo-600">
             <FaTwitter />
             <a
-              className="twitter-follow-button"
+              className="linkedin-follow-button"
               rel="noopener noreferrer"
               target="_blank"
-              href="https://twitter.com/soumyajit4419"
+              href="https://www.linkedin.com/in/sarthakpaneru/"
             >
               Follow Me
             </a>
